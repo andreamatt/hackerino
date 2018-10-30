5e42a52f2142afb1b0561cdaf3a1a0938d96cb3d
Andrei Diaconu <andrei.diaconu@studenti.unitn.it>
2018-10-30 18:43:56 +0100

    added app.get('/') and app.listen()

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var port = 3000
+var express = require("express");
+
+var port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+
+});
+
+app.listen(port, () => console.log("server running"));
