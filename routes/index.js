1aa01e01c371789f95733a4b070633b59e7a52ef
andreamatt <andreamatte97@gmail.com>
2018-11-26 17:42:01 +0100

    fixed old requires

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,5 @@
 // import different routes
-var bodyParser = require('body-parser'),
-	exams = require('./exams'),
-	reviews = require('./reviews'),
-	submissions = require('./submissions'),
-	teachers = require('./teachers'),
-	students = require('./students'),
-	tasks = require('./tasks');
+var bodyParser = require('body-parser');
 
 // basic setup
 const app = require('express')();
