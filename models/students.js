b28a63fe0430d675375afad34bd9a0aa455fd670
Andrea Mattè <andreamatte97@gmail.com>
2018-12-03 20:33:07 +0100

    added students_POST
    fixed students_GET limit

--- models/students.js
+++ models/students.js
@@ -7,20 +7,20 @@ const doLimit = util.doLimit;
 ////////////////////////////////////////////////////
 const students_list = {};
 
-function Student(email, firstname, lastname) {
+function Student(email, first_name, last_name) {
 	this.id = 1;
-	while (students_list[id]) {
+	while (students_list[this.id]) {
 		this.id++;
 	}
 	this.email = email;
-	this.firstname = firstname;
-	this.lastname = lastname;
+	this.first_name = first_name;
+	this.last_name = last_name;
 }
 
 function students_GET(req) {
 	let result = Object.values(students_list);
 	let email = req.query.email;
-	if (email) {
+	if (email !== undefined) {
 		if (!isString(email)) {
 			return new Response(400, "Bad email query", null);
 		}
@@ -28,7 +28,7 @@ function students_GET(req) {
 	}
 	let tot = result.length;
 	let offset = req.query.offset;
-	if (offset) {
+	if (offset !== undefined) {
 		if (!isInteger(offset)) {
 			return new Response(400, "Bad offset query", null);
 		}
@@ -38,7 +38,7 @@ function students_GET(req) {
 		result = doOffset(result, offset);
 	}
 	let limit = req.query.limit;
-	if (limit) {
+	if (limit !== undefined) {
 		if (!isInteger(limit)) {
 			return new Response(400, "Bad limit query", null);
 		}
@@ -50,4 +50,22 @@ function students_GET(req) {
 	return new Response(200, null, { tot_students: tot, students: result });
 }
 
-module.exports = { students_GET };
+function students_POST(req) {
+	let email = req.body.email;
+	let first_name = req.body.first_name;
+	let last_name = req.body.last_name;
+	if (!isString(email)) return new Response(400, "Bad email body parameter", null);
+	if (!isString(first_name)) return new Response(400, "Bad first_name body parameter", null);
+	if (!isString(last_name)) return new Response(400, "Bad last_name body parameter", null);
+
+	let withEmail = Object.values(students_list).filter(stud => stud.email === email);
+	if (withEmail.length > 0) {
+		return new Response(423, "A student with such email already exists", null);
+	}
+
+	let stud = new Student(email, first_name, last_name);
+	students_list[stud.id] = stud;
+	return new Response(201, null, stud);
+}
+
+module.exports = { students_GET, students_POST };
--- models/tests/students.test.js
+++ models/tests/students.test.js
@@ -1,22 +1,247 @@
 const students = require('../students');
 const util = require('../utility');
 const Request = util.Request;
-const Response = util.Response;
 const students_GET = students.students_GET;
+const students_POST = students.students_POST;
 
-describe("/students GET empty", () => {
-	test("With no query", () => {
-		let request = new Request();
-		let response = students_GET(request);
-		expect(response.json.tot_students).toBe(0);
-		expect(response.json.students).toEqual([]);
-	});
-	test("With good limit and offset", () => {
-		let request = new Request();
-		request.query = { limit: 2, offset: 9 };
-		let response = students_GET(request);
-		expect(response.json.tot_students).toBe(0);
-		expect(response.json.students).toEqual([]);
+beforeAll(() => {
+	describe("/students GET empty", () => {
+		test("With no query", () => {
+			let request = new Request();
+			let response = students_GET(request);
+			expect(response.status).toBe(200);
+			expect(response.text).toBeNull();
+			expect(response.json.tot_students).toBe(0);
+			expect(response.json.students).toEqual([]);
+		});
+		test("With good limit and offset", () => {
+			let request = new Request();
+			request.query = { limit: 2, offset: 9 };
+			let response = students_GET(request);
+			expect(response.status).toBe(200);
+			expect(response.text).toBeNull();
+			expect(response.json.tot_students).toBe(0);
+			expect(response.json.students).toEqual([]);
+		});
 	});
 });
 
+describe("/students POST and GET", () => {
+	test("Add twice and get it", () => {
+		let request = new Request();
+		request.body = { email: "a.b@c", first_name: "a", last_name: "b" };
+		let response = students_POST(request);
+		expect(response.status).toBe(201);
+		expect(response.text).toBeNull();
+		expect(response.json).toMatchObject({
+			email: "a.b@c",
+			first_name: "a",
+			last_name: "b"
+		});
+
+		response = students_POST(request);
+		expect(response.status).toBe(423);
+		expect(response.json).toBeNull();
+		expect(util.isString(response.text)).toBe(true);
+
+		request = new Request();
+		request.query = { email: "a.b@c" };
+		response = students_GET(request);
+		expect(response.status).toBe(200);
+		expect(response.text).toBeNull();
+		expect(response.json).toMatchObject({
+			tot_students: 1,
+			students: [{
+				email: "a.b@c",
+				first_name: "a",
+				last_name: "b"
+			}]
+		});
+
+		request = new Request();
+		request.query = { email: "a.b@c", limit: 1, offset: 0 };
+		response = students_GET(request);
+		expect(response.status).toBe(200);
+		expect(response.text).toBeNull();
+		expect(response.json).toMatchObject({
+			tot_students: 1,
+			students: [{
+				email: "a.b@c",
+				first_name: "a",
+				last_name: "b"
+			}]
+		});
+
+		request = new Request();
+		request.query = { email: "a.b@c", limit: 0 };
+		response = students_GET(request);
+		expect(response.status).toBe(200);
+		expect(response.text).toBeNull();
+		expect(response.json).toEqual({
+			tot_students: 1,
+			students: []
+		});
+
+		request = new Request();
+		request.query = { email: "a.b@c", offset: 1 };
+		response = students_GET(request);
+		expect(response.status).toBe(200);
+		expect(response.text).toBeNull();
+		expect(response.json).toEqual({
+			tot_students: 1,
+			students: []
+		});
+
+		request = new Request();
+		request.query = { email: "a.b@c", offset: -1 };
+		response = students_GET(request);
+		expect(response.status).toBe(400);
+		expect(util.isString(response.text)).toBe(true);
+		expect(response.json).toBeNull();
+
+		request = new Request();
+		request.query = { email: "a.b@c", limit: -1 };
+		response = students_GET(request);
+		expect(response.status).toBe(400);
+		expect(util.isString(response.text)).toBe(true);
+		expect(response.json).toBeNull();
+	});
+});
+
+describe("students heavy POST and GET", () => {
+	for (let i = 0; i < 100; i++) {
+		let request = new Request();
+		request.body = { email: i.toString(), first_name: "a", last_name: "b" };
+		let response = students_POST(request);
+		expect(response.status).toBe(201);
+		expect(response.text).toBeNull();
+		expect(util.isStudent(response.json)).toBe(true);
+	}
+
+	let request = new Request();
+	request.query = {};
+	let response = students_GET(request);
+	expect(response.status).toBe(200);
+	expect(response.text).toBeNull();
+	expect(response.json.tot_students).toBe(response.json.students.length);
+	let students = response.json.students;
+	expect(students.every(stud => util.isStudent(stud))).toBe(true);
+
+	request = new Request();
+	request.query = { email: "1" };
+	response = students_GET(request);
+	expect(response.status).toBe(200);
+	expect(response.text).toBeNull();
+	expect(response.json.tot_students).toBe(1);
+
+	request = new Request();
+	request.query = { limit: "1" };
+	response = students_GET(request);
+	expect(response.status).toBe(400);
+
+	request = new Request();
+	request.query = { limit: 20 };
+	response = students_GET(request);
+	expect(response.status).toBe(200);
+	expect(response.text).toBeNull();
+	expect(response.json.tot_students >= 100).toBe(true);
+	expect(response.json.students.length).toBe(20);
+
+	request = new Request();
+	request.query = { limit: 20, offset: 50 };
+	response = students_GET(request);
+	expect(response.status).toBe(200);
+	expect(response.text).toBeNull();
+	expect(response.json.tot_students >= 100).toBe(true);
+	expect(response.json.students.length).toBe(20);
+	students = response.json.students;
+	expect(students.every(stud => stud.id >= 50)).toBe(true);
+});
+
+describe("students GET with wrong parameters", () => {
+	let request = new Request();
+	request.query = { email: "" };
+	let response = students_GET(request);
+	expect(response.status).toBe(400);
+
+	request = new Request();
+	request.query = { email: 1 };
+	response = students_GET(request);
+	expect(response.status).toBe(400);
+
+	request = new Request();
+	request.query = { limit: -1 };
+	response = students_GET(request);
+	expect(response.status).toBe(400);
+
+	request = new Request();
+	request.query = { offset: -1 };
+	response = students_GET(request);
+	expect(response.status).toBe(400);
+
+	request = new Request();
+	request.query = { limit: "1" };
+	response = students_GET(request);
+	expect(response.status).toBe(400);
+
+	request = new Request();
+	request.query = { offset: {} };
+	response = students_GET(request);
+	expect(response.status).toBe(400);
+});
+
+describe("students POST with bad parameters", () => {
+	let request = new Request();
+	request.body = {
+		email: 1,
+		first_name: "a",
+		last_name: "b"
+	};
+	let response = students_POST(request);
+	expect(response.status).toBe(400);
+	expect(util.isString(response.text)).toBe(true);
+	expect(response.json).toBeNull();
+
+	request = new Request();
+	request.body = {
+		first_name: "a",
+		last_name: "b"
+	};
+	response = students_POST(request);
+	expect(response.status).toBe(400);
+	expect(util.isString(response.text)).toBe(true);
+	expect(response.json).toBeNull();
+
+	request = new Request();
+	request.body = {
+		email: "",
+		first_name: "a",
+		last_name: "b"
+	};
+	response = students_POST(request);
+	expect(response.status).toBe(400);
+	expect(util.isString(response.text)).toBe(true);
+	expect(response.json).toBeNull();
+
+	request = new Request();
+	request.body = {
+		email: "asd",
+		first_name: 1,
+		last_name: "b"
+	};
+	response = students_POST(request);
+	expect(response.status).toBe(400);
+	expect(util.isString(response.text)).toBe(true);
+	expect(response.json).toBeNull();
+
+	request = new Request();
+	request.body = {
+		email: "asd",
+		first_name: "a",
+		last_name: null
+	};
+	response = students_POST(request);
+	expect(response.status).toBe(400);
+	expect(util.isString(response.text)).toBe(true);
+	expect(response.json).toBeNull();
+});
