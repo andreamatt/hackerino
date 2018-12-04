67bec862f89ec323f3fe0e72e62ff46f7146e371
Andrei <andrei.diaconu@studenti.unitn.it>
2018-12-04 22:06:27 +0100

    Add reviews
    Added:
        - Review constructor
        - isReview in utility
        - createReview
        - reviews_GET
        - reviews_POST
        - reviews_reviewID_GET
        - reviews_reviewID_PUT
        - reviews_reviewID_DELETE
    
    Buxfixes:
        - fixed query filtering order
        - fixed createReview() update condition
        - re-added curly braces for simple if cases
    
    Pending:
        - waiting for dependencies
        - tests

--- models/utility.js
+++ models/utility.js
@@ -28,45 +28,6 @@ function isArray(value) {
 	return false;
 }
 
-function isTask(task) {
-	if (!task) return "Task is falsy";
-	if (!isInteger(task.id)) return "Task id is not an integer";
-	if (task.id <= 0) return "Task id is negative";
-
-	if (!task.question) return "Question is falsy";
-	if (!isString(task.question)) return "Question is not a string or an empty one";
-
-	if (task.answers) {
-		if (Object.keys(task.answers).length > 2) return "Answers has invalid number of properties";
-
-		if (!task.answers.possible_answers) return "Possible_answers is falsy";
-		if (!isArray(task.answers.possible_answers)) return "Possible_answers is not an array";
-		if (task.answers.possible_answers.length < 2) return "Possible_answers must have at least two answers";
-		if (!task.answers.possible_answers.every(possible_answer => isString(possible_answer))) {
-			return "Some possible answers are not strings or are empty strings";
-		}
-
-		if (!task.answers.correct_answers) return "Correct_answers is falsy";
-		if (!isArray(task.answers.correct_answers)) return "Correct_answers is not an array";
-		if (task.answers.correct_answers.length < 1) return "There must be at least one correct answer";
-		if (!task.answers.correct_answers.every(correct_answer => isInteger(correct_answer))) {
-			return "Some correct_answers indices are not integers";
-		}
-		if (!task.answers.correct_answers.every(correct_answer => correct_answer >= 0)) {
-			return "Some correct_answers indices are negative";
-		}
-		if (!task.answers.correct_answers.every(correct_answer => correct_answer < task.answers.possible_answers.length)) {
-			return "Some correct_answers indices are out of range";
-		}
-	}
-	if (!isInteger(task.n_votes) || task.n_votes < 0) return "N_votes is not an integer";
-	if (!isNumber(task.rating)) return "Rating is not a number";
-	if (task.rating < 0 || task.rating > 10) return "Rating is out of range";
-	if (Object.keys(task).length !== 4 && Object.keys(task).length !== 5) return "Task has invalid number of properties";
-
-	return true;
-}
-
 function doOffset(collection, offset) {
 	if (!isArray(collection) || !isInteger(offset) || offset < 0) {
 		throw new Error("Bad doOffset parameters");
@@ -139,4 +100,63 @@ function isExam(exam) {
 	return "ok";
 }
 
-module.exports = { toInt, isInteger, isString, isNumber, isStringDate, isArray, isStudent, isTeacher, isTask, isExam, doOffset, doLimit, doOffsetLimit, Request, Response };
+function isTask(task) {
+	if (!task) return "Task is falsy";
+	if (!isInteger(task.id)) return "Task id is not an integer";
+	if (task.id <= 0) return "Task id is negative";
+
+	if (!task.question) return "Question is falsy";
+	if (!isString(task.question)) return "Question is not a string or an empty one";
+
+	if (task.answers) {
+		if (Object.keys(task.answers).length > 2) return "Answers has invalid number of properties";
+
+		if (!task.answers.possible_answers) return "Possible_answers is falsy";
+		if (!isArray(task.answers.possible_answers)) return "Possible_answers is not an array";
+		if (task.answers.possible_answers.length < 2) return "Possible_answers must have at least two answers";
+		if (!task.answers.possible_answers.every(possible_answer => isString(possible_answer))) {
+			return "Some possible answers are not strings or are empty strings";
+		}
+
+		if (!task.answers.correct_answers) return "Correct_answers is falsy";
+		if (!isArray(task.answers.correct_answers)) return "Correct_answers is not an array";
+		if (task.answers.correct_answers.length < 1) return "There must be at least one correct answer";
+		if (!task.answers.correct_answers.every(correct_answer => isInteger(correct_answer))) {
+			return "Some correct_answers indices are not integers";
+		}
+		if (!task.answers.correct_answers.every(correct_answer => correct_answer >= 0)) {
+			return "Some correct_answers indices are negative";
+		}
+		if (!task.answers.correct_answers.every(correct_answer => correct_answer < task.answers.possible_answers.length)) {
+			return "Some correct_answers indices are out of range";
+		}
+	}
+	if (!isInteger(task.n_votes) || task.n_votes < 0) return "N_votes is not an integer";
+	if (!isNumber(task.rating)) return "Rating is not a number";
+	if (task.rating < 0 || task.rating > 10) return "Rating is out of range";
+	if (Object.keys(task).length !== 4 && Object.keys(task).length !== 5) return "Task has invalid number of properties";
+
+	return true;
+}
+
+function isReview(review) {
+	if (!review) return "Review is falsy";
+
+	if (!isInteger(review.id)) return "Review id is not an integer";
+	if (review.id < 1) return "Review id is not valid integer";
+
+	if (!isInteger(review.studentID)) return "StudentID is not an integer";
+	if (review.studentID < 1) return "StudentID is not valid integer";
+
+	if (!isInteger(review.submissionID)) return "SubmissionID is not an integer";
+	if (review.submissionID < 1) return "SubmissionID is not valid integer";
+
+	if (!isInteger(review.mark)) return "Mark is not an integer";
+	if (review.mark < 0 || review.mark > 30) return "Mark is out of range";
+
+	if (Object.keys(review).length !== 4) return "Review has invalid number of properties";
+
+	return true;
+}
+
+module.exports = { toInt, isInteger, isString, isNumber, isStringDate, isArray, isStudent, isTeacher, isTask, isReview, isExam, doOffset, doLimit, doOffsetLimit, Request, Response };
